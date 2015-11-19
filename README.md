9990dcecafcdd64b0f5c505204c1eecb9efd28d0
jiancarlo <jvale009@odu.edu>
2015-11-18 20:46:01 -0500

    Finished ToDo functionality then messed with some angular-material

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # angular-todo
+
+learning angular
--- main.js
+++ main.js
@@ -1,28 +1,27 @@
-angular.module('todoApp', [])
-  .controller('toDoController', function($scope) {
-    $scope.tasks = [
-      {
-      text: 'take out the dog'
-    },{
-      text: 'clean dishes'
-    }
-    ];
+angular.module('todoApp', ['ngMaterial'])
+      .controller('toDoController', function($scope) {
+            $scope.tasks = [
+            {
+                  text: 'Be the best in school'
+            },{
+                  text: 'Catch all the balls at baseball practice'
+            },{
+                  text: 'Train dog to sit'
+            },{
+                  text: 'Travel across campus'
+            },{
+                  text: 'Search for lost camera'
+            },{
+                  text: 'Understand angular'
+            }
+            ];
 
-    $scope.completedTasks = [
-      {
-      text: 'do homework'
-    },{
-      text: 'run a mile'
-    }
-  ];
+            $scope.addTask = function() {
+                  $scope.tasks.push({text:$scope.taskText});
+                  $scope.taskText ='';
+            }
 
-
-    $scope.addTask = function() {
-      $scope.tasks.push({text:$scope.taskText});
-      $scope.taskText ='';
-    }
-
-    $scope.checked = function(){
-        $scope.completedTasks.push({text:$scope.task.text});
-    }
-  });
+            $scope.checked = function($index){
+                  $scope.tasks.splice($index, 1);
+            }
+      });
